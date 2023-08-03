07fca951a059b658970975fd88301d8edfc3ac33
Milk-Cool <milan-pl@mail.ru>
2023-08-03 15:04:08 +0300

    added web login support

--- index.js
+++ index.js
@@ -7,9 +7,18 @@ const { question } = require("readline-sync");
 const cluster = require("cluster");
 const { availableParallelism } = require("os");
 const IPCIDR = require("ip-cidr");
+const puppeteer = require("puppeteer");
 
 const args = parseArgs({
 	"options": {
+        "telnet": {
+            "type": "boolean",
+            "short": "t"
+        },
+        "web": {
+            "type": "boolean",
+            "short": "w"
+        },
 		"outfile": {
 			"type": "string",
 			"short": "o"
@@ -20,11 +29,15 @@ const args = parseArgs({
         },
         "timeout": {
             "type": "string",
-            "short": "t"
+            "short": "T"
+        },
+        "timeoutweb": {
+            "type": "string",
+            "short": "Q"
         },
         "workers": {
             "type": "string",
-            "short": "w"
+            "short": "W"
         },
 		"help": {
 			"type": "boolean",
@@ -33,11 +46,16 @@ const args = parseArgs({
 	}
 });
 
+const TELNET = args.values.telnet;
+const WEB = args.values.web;
 const OUTFILE = args.values.outfile;
 const INFILE = args.values.infile;
 const TIMEOUT = parseInt(args.values.timeout ?? 500);
+const TIMEOUTWEB = parseInt(args.values.timeoutweb ?? 1500);
 const WORKERS = parseInt(args.values.workers ?? availableParallelism());
 const HELP = args.values.help;
+
+const sleep = delay => new Promise(resolve => setTimeout(resolve, delay));
 
 if(HELP) {
     console.log(`
@@ -45,14 +63,17 @@ Improved adminadmin finder - Milk_Cool, 2023
 
 Usage:
 adminadmin --help
-adminadmin [-i INFILE] [-o OUTFILE] [-t TIMEOUT] [-w WORKERS]
+adminadmin [-t] [-w] [-i INFILE] [-o OUTFILE] [-t TIMEOUT] [-w WORKERS]
 
 Arguments:
--h, --help    Prints help and exits.
--i, --infile  Defines the file to take IP addresses from. Also accepts wildcard IPs such as 1.2.*.*.
--o, --outfile Defines the file to write vulnerable routers to. Overwrites the contents of the file.
--t, --timeout Defines the connection timeout (default: 500)
--w, --workers Defines the amount of workers to fork (default: no. of CPU cores)
+-t, --telnet      Try to authenticate with Telnet (port 23)
+-w, --web         Try to authenticate with Chrome (port 80)
+-h, --help        Prints help and exits.
+-i, --infile      Defines the file to take IP addresses from. Also accepts wildcard IPs such as 1.2.*.*.
+-o, --outfile     Defines the file to write vulnerable routers to. Overwrites the contents of the file.
+-T, --timeout     Defines the Telnet connection timeout (default: 500)
+-Q, --timeoutweb  Defines the Chrome connection timeout (default: 1500)
+-W, --workers     Defines the amount of workers to fork (default: no. of CPU cores)
 `);
 	process.exit(0);
 }
@@ -77,26 +98,41 @@ const processIPs = (pattern, depth = 0, list = []) => {
     return list.concat([pattern]);
 }
 
-const testOne = async host => {
-    const connection = new Telnet();
-    const params = {
-        host,
-        "port": 23,
-        "timeout": TIMEOUT,
-        "login": "admin",
-        "password": "admin"
-    };
+const testOne = async (host, browser) => {
+    if(TELNET) {
+        const connection = new Telnet();
+        const params = {
+            host,
+            "port": 23,
+            "timeout": TIMEOUT,
+            "login": "admin",
+            "password": "admin"
+        };
 
-    try {
-        await connection.connect(params);
-        await connection.end();
-        await connection.destroy();
-        cluster.worker.send("+" + host);
-        return true;
-    } catch(_) {
-        cluster.worker.send("-" + host);
-        return false;
+        try {
+            await connection.connect(params);
+            await connection.end();
+            await connection.destroy();
+            cluster.worker.send("+" + host);
+            return true;
+        } catch(_) { }
     }
+    if(WEB) {
+        try {
+            const page = await browser.newPage();
+            await page.goto(`http://${host}:80`, { "timeout": TIMEOUTWEB });
+            await page.type("input[autocomplete=\"username\"],input[name=\"user\"],input[name=\"username\"],input[name=\"login\"],input[name=\"loginLogin\"],input[type=\"text\"]", "admin");
+            await page.type("input[autocomplete=\"password\"],input[type=\"password\"],input[name=\"pass\"],input[name=\"passphrase\"],input[name=\"password\"]", "admin");
+            await page.click("input[type=\"submit\"],button[type=\"submit\"],button");
+            const res = await page.waitForNavigation({ "timeout": TIMEOUTWEB });
+            if(res.status().toString()[0] == "2") {
+                cluster.worker.send("+" + host);
+                return true;
+            }
+        } catch(_) { }
+    }
+    cluster.worker.send("-" + host);
+    return false;
 };
 
 const main = async () => {
@@ -114,8 +150,9 @@ const main = async () => {
     const m = n;
     for(let i = 0; i < Math.min(WORKERS, listFinal.length); i++) {
         const worker = cluster.fork();
-        worker.on("online", () => {
+        worker.on("online", async () => {
             console.log(`Worker #${worker.id} online!`);
+            await sleep(1000);
             worker.send(listFinal.pop());
         });
         worker.on("message", msg => {
@@ -139,7 +176,11 @@ const main = async () => {
 }
 
 const mainWorker = async () => {
-    process.on("message", testOne);
+    let browser = null;
+    if(WEB) {
+        browser = await puppeteer.launch({ "headless": "new" });
+    }
+    process.on("message", msg => testOne(msg, browser));
 };
 
 if(cluster.isPrimary)
